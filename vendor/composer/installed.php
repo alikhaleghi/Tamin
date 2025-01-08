d24768705fd899ed2f2abb799ca76f8e1f9102c8
Ali Khaleghi <awli.khaleghi@gmail.com>
2025-01-08 04:51:19 +0330

    +

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alikhaleghi/tamin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cfe21201f129164e96efe5adb21e33d810b96301',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alikhaleghi/tamin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cfe21201f129164e96efe5adb21e33d810b96301',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
