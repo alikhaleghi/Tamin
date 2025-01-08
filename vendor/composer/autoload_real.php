8541383376bfce07e7f121524d0c5e947c48368a
Ali Khaleghi <awli.khaleghi@gmail.com>
2025-01-08 05:06:34 +0330

    +

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit79b7b1d44aeaa37541a88cdfd3e0bd18
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit79b7b1d44aeaa37541a88cdfd3e0bd18::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
